380aae49717255c5082488b775806cb574d69fee
Karina <36827712+Karina10135@users.noreply.github.com>
2018-05-03 13:54:37 +1200

    Pick up and Drop Object

---
+++ Assets/Components/Scripts/PickupManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour
+{
+
+    public GameObject heldObject;
+    public Transform holdPos;
+    public bool holding;
+    public float maxDistance;
+    Color debugColor = Color.red;
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        if(Physics.Raycast(ray, out hit, maxDistance))
+        {
+            if (hit.collider.gameObject.CompareTag("Object"))
+            {
+                debugColor = Color.green;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (!holding)
+                    {
+                        Pickup(hit.collider.gameObject);
+                        return;
+                    }
+
+                }
+            }
+            
+        }
+        else debugColor = Color.red;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+
+            if (holding)
+            {
+                Drop();
+                return;
+            }
+
+        }
+
+
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 4, debugColor);
+    }
+
+    void Pickup(GameObject obj) //Pick up tagged object
+    {
+        heldObject = obj;
+        heldObject.GetComponent<Rigidbody>().isKinematic = true;
+        heldObject.transform.parent = holdPos;
+        heldObject.transform.position = holdPos.position;
+        holding = true;
+    }
+
+    void Drop() //Simply drop held object.
+    {
+        print("<color=red>Dropped Object</color>");
+        heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        heldObject.transform.SetParent(null);
+        heldObject = null;
+        holding = false;
+    }
+
+    void PlaceLetter()
+    {
+
+    }
+
+    
+
+}
