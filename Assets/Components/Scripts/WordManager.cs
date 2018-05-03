8055fa0baa47a8dacd2fa11d6a02c3b4ee44c5c8
Karina <36827712+Karina10135@users.noreply.github.com>
2018-05-03 14:08:29 +1200

    Character split in words

---
+++ Assets/Components/Scripts/WordManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour
+{
+    public string word;
+    public string[] characters;
+
+    private void Start()
+    {
+        
+        characters = new string[word.Length];
+        for (int i = 0; i < word.Length; i++)
+        {
+            characters[i] = word[i].ToString();
+            print(word[i]);
+        }
+    }
+
+}
