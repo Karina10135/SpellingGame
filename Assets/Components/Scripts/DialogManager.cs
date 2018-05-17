b41de3017d09856491cf4d3f452cdb7ed0487d9c
Karina <36827712+Karina10135@users.noreply.github.com>
2018-05-17 14:37:59 +1200

    Dialog setup

---
+++ Assets/Components/Scripts/DialogManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public string name;
+
+    public Text characterName;
+    public Text dialogueText;
+    public int step;
+
+    [TextArea(3,10)]
+    public string[] sentences;
+    bool dialog;
+
+    public void Start()
+    {
+        characterName.text = name.ToString();
+        //StartCoroutine(StartDialogTimer());
+        StartDialog();
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            NextDialog();
+        }
+    }
+
+
+    public void StartDialog()
+    {
+        dialog = true;
+        
+        StartCoroutine(TypeSentence(sentences[0]));
+    }
+
+    public void NextDialog()
+    {
+        if (!dialog) { return; }
+        StopAllCoroutines();
+
+        if (step < sentences.Length)
+        {
+            step++;
+            StartCoroutine(TypeSentence(sentences[step]));
+
+        }
+
+    }
+
+    IEnumerator StartDialogTimer()
+    {
+        yield return new WaitForSeconds(3);
+        StartDialog();
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+}
